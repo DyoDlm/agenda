4947f330191840d6ff4e8b2c26a3df81d1f31897
dyodlm <baccus.delamancha@gmail.com>
2026-02-16 10:50:50 +0100

    phy

---
+++ foo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int	main()
+{
+	char	*s = "doo";
+
+	printf("Look at my beautifull string : %s\n", s);
+	return 0;
+}
---
+++ foo.js
@@ -0,0 +1,13 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Well, hello...")
+	}
+}
+i
+ii
+iii
+iiii
+iiiii
+iiiiii
+iiiiiii
+iiiiiiii
